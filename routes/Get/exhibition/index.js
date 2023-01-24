0a357702d0ebfb35ba7db46b486a8ef50c181961
fengxintian <3466117078@qq.com>
2023-01-24 21:16:14 +0800

    0.0.1

--- routes/Get/exhibition/index.js
+++ routes/Get/exhibition/index.js
@@ -4,6 +4,10 @@ const { commonAutoGun, getSqlBasicInfo } = require("../../../untils");
 const querySql = require("../../../mysql");
 const router = express.Router();
 
+const questErr = {
+  code: 400,
+  message: "请稍后重试"
+};
 
 //  /home
 router.get('/home', async function (_, res) {
@@ -18,12 +22,16 @@ router.get('/home', async function (_, res) {
   i >= recommendList.length ? i = recommendList.length - 1 : i = i;
 
   // 使用过滤黑名单方法 先过滤一遍 再返回
-  obj["recommendList"] = await getSqlBasicInfo(recommendList[i]);
+  try {
+    obj["recommendList"] = await getSqlBasicInfo(recommendList[i]);
 
-  const MovieInfo = serverObj.get("MovieInfo");
-  obj["MovieInfo"] = MovieInfo;
+    const MovieInfo = serverObj.get("MovieInfo");
+    obj["MovieInfo"] = MovieInfo;
 
-  res.send({ ...obj, code: 200 });
+    res.send({ ...obj, code: 200 });
+  } catch(err) {
+    res.send(questErr);
+  }
 });
 
 // /classify
@@ -59,61 +67,65 @@ router.post("/classify", async (req, res) => {
       classify_name = null;
   };
   let obj = {};
-  // 不穿classify_name会将所有分类的数据都返回
-  if (!classify_name) {
-    function* gen() {
-      for (let i = 1; i < publishList.length; i++) {
-        yield publishList[i];
-      }
-    };
-    commonAutoGun(gen(), async (value, run) => {
+  try {
+    // 不穿classify_name会将所有分类的数据都返回
+    if (!classify_name) {
+      function* gen() {
+        for (let i = 1; i < publishList.length; i++) {
+          yield publishList[i];
+        }
+      };
+      commonAutoGun(gen(), async (value, run) => {
+        const [{ "count(id)": len }] = await querySql(`
+          SELECT count(id) from basic_info
+          WHERE type="${value}" 
+          AND basic_info.id 
+          NOT IN (SELECT id from black_list_movie)
+        `);
+        const arr = await querySql(`
+          SELECT * from basic_info
+          WHERE type="${value}" 
+          AND basic_info.id 
+          NOT IN (SELECT id from black_list_movie)
+          ORDER BY ${type} ${sort}
+          limit ${limit} offset ${offset}
+        `);
+        obj[value] = {
+          renderList: arr,
+          allListLen: len
+        };
+        run();
+      }, () => res.send({
+        ...obj,
+        code: 200
+      }));
+    } else {
       const [{ "count(id)": len }] = await querySql(`
         SELECT count(id) from basic_info
-        WHERE type="${value}" 
+        WHERE type="${classify_name}" 
         AND basic_info.id 
         NOT IN (SELECT id from black_list_movie)
       `);
       const arr = await querySql(`
         SELECT * from basic_info
-        WHERE type="${value}" 
+        WHERE type="${classify_name}" 
         AND basic_info.id 
         NOT IN (SELECT id from black_list_movie)
         ORDER BY ${type} ${sort}
         limit ${limit} offset ${offset}
       `);
-      obj[value] = {
+      obj[classify_name] = {
         renderList: arr,
         allListLen: len
       };
-      run();
-    }, () => res.send({
-      ...obj,
-      code: 200
-    }));
-  } else {
-    const [{ "count(id)": len }] = await querySql(`
-      SELECT count(id) from basic_info
-      WHERE type="${classify_name}" 
-      AND basic_info.id 
-      NOT IN (SELECT id from black_list_movie)
-    `);
-    const arr = await querySql(`
-      SELECT * from basic_info
-      WHERE type="${classify_name}" 
-      AND basic_info.id 
-      NOT IN (SELECT id from black_list_movie)
-      ORDER BY ${type} ${sort}
-      limit ${limit} offset ${offset}
-    `);
-    obj[classify_name] = {
-      renderList: arr,
-      allListLen: len
+      res.send({
+        ...obj,
+        code: 200
+      })
     };
-    res.send({
-      ...obj,
-      code: 200
-    })
-  };
+  } catch(err) {
+    res.send(questErr)
+  }
 });
 
 // /specific
@@ -148,43 +160,50 @@ router.post("/specific", async (req, res) => {
 // /search
 router.post("/search", async (req, res) => {
   const { name = "", limit = "30", offset = "0" } = req.body;
-  const [{ "count(id)": len }] = await querySql(`
-    SELECT count(id) from basic_info
-    WHERE name like '%${name}%'
-    AND id NOT IN
-    (SELECT id from black_list_movie)
-  `);
-  const sqlStr = `
-    SELECT * from basic_info
-    WHERE name like '%${name}%'
-    AND id NOT IN
-    (SELECT id from black_list_movie)
-    ORDER BY hot ASC
-    limit ${limit} offset ${offset}
-  `;
-  const arr = await querySql(sqlStr);
-  res.send({
-    renderList: arr,
-    allListLen: len,
-    code: 200
-  })
+  try {
+    const [{ "count(id)": len }] = await querySql(`
+      SELECT count(id) from basic_info
+      WHERE name like '%${name}%'
+      AND id NOT IN
+      (SELECT id from black_list_movie)
+    `);
+    const sqlStr = `
+      SELECT * from basic_info
+      WHERE name like '%${name}%'
+      AND id NOT IN
+      (SELECT id from black_list_movie)
+      ORDER BY hot ASC
+      limit ${limit} offset ${offset}
+    `;
+    const arr = await querySql(sqlStr);
+    res.send({
+      renderList: arr,
+      allListLen: len,
+      code: 200
+    })
+  } catch(err) {
+    res.send(questErr)
+  }
 })
 
 // /search_lenovo
 router.post("/search_lenovo", async (req, res) => {
   const { name = "" } = req.body;
-  const wordList = await querySql(`
-    SELECT name,id from basic_info
-    WHERE name LIKE "%${name}%"
-    AND id NOT IN
-    (SELECT id from black_list_movie)
-    ORDER BY hot ASC LIMIT 8
-  `);
-  console.log(name)
-  res.send({
-    wordList,
-    code: 200
-  })
+  try {
+    const wordList = await querySql(`
+      SELECT name,id from basic_info
+      WHERE name LIKE "%${name}%"
+      AND id NOT IN
+      (SELECT id from black_list_movie)
+      ORDER BY hot ASC LIMIT 8
+    `);
+    res.send({
+      wordList,
+      code: 200
+    })
+  } catch(err) {
+    res.send(questErr)
+  }
 });
 
 module.exports = router;
